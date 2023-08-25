e76c0821e75b145ee98bd39b226539d3c46867ef
SushantShrestha12 <88120835+SushantShrestha12@users.noreply.github.com>
2023-08-25 15:44:14 +0545

    Add files via upload

--- Resort.Domain/Payment/Payment.cs
+++ Resort.Domain/Payment/Payment.cs
@@ -10,19 +10,15 @@ public class Payment : AggregateRoot<Guid>
     }
     
     public string Date { get; private set; }
-    public string RoomStayed { get; private set; }
-    public string TotalStay { get; private set; }
     public string Price { get; private set; }
     public string Total { get; private set; }
     public string Discount { get; private set; }
     public string GrandTotal { get; private set; }
 
-    public Payment(Guid id,string date, string roomStayed, string totalStay, string price,
+    public Payment(Guid id,string date, string price,
         string total, string discount, string grandTotal)
     {
         Date = date;
-        RoomStayed = roomStayed;
-        TotalStay = totalStay;
         Price = price;
         Total = total;
         Discount = discount;
