ef70a34453f54bee40b2e589150ba1b1cf4a134e
SushantShrestha12 <88120835+SushantShrestha12@users.noreply.github.com>
2023-09-29 13:34:06 +0545

    Add files via upload

---
+++ Resort.Domain/Tokens/AccessToken.cs
@@ -0,0 +1,33 @@
+using System.Runtime.InteropServices.JavaScript;
+using Resort.Domain.SharedKernel;
+
+namespace Resort.Domain.Token;
+
+public class AccessToken: AggregateRoot<Guid>
+{
+    private AccessToken()
+    {
+        
+    }
+
+    public string Username { get; private set; }
+    public string AccToken { get;  private set; }
+    public DateTime AccExpires { get; private set; }
+    public string RefreshToken { get; private set; }
+    public DateTime RefreshExpires { get; private set; }
+    
+    public AccessToken(string username, string accToken, DateTime accExpires, string refreshToken, DateTime refreshExpires)
+    {
+        Username = username;
+        AccToken = accToken;
+        AccExpires = accExpires;
+        RefreshToken = refreshToken;
+        RefreshExpires = refreshExpires;
+    }
+    
+    public void setAccExpire(DateTime accExpire)
+    {
+        AccExpires = accExpire;
+    }
+    
+}
